2ac13667d0e306d560c4f7edc19d10e66fd33538
Chuan Lin 2025 <1304404717@qq.com>
2025-07-02 10:20:18 -0400

    July 2

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ from problems import *
 from distances import *
 from scipy.spatial import distance
 
+
 # Step 1: Get data
 n_samples = 100
 X_source, y_source, X_target, y_target = create_domain_adaptation_problem(n_samples=n_samples, noise_level=0.1)
-
 
 # Step 2: Set hyper-parameter
 hyper_parameter_n_classes = 2
 hyper_parameter_p = 2
 hyper_parameter_c = 2
-learning_rate = 0.01
+learning_rate = 0.001
 num_epochs = 5000
-num_epochs_per_print = 1000
+num_epochs_per_print = 20
 speed_up_options = {"msg":False}
 list_of_num_hidden_units = [16]
 model = SimpleClassifier(list_of_num_hidden_units)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+ideal_causal_distance, _ = calculate_causal_distance_between_datasets(
+            X_source, y_source, X_target, y_target, hyper_parameter_n_classes,
+            order_parameter_p=hyper_parameter_p, scaling_parameter_c=hyper_parameter_c, options=speed_up_options
+        )
 
 
 # Step 3: Train Model
@@ -32,24 +36,28 @@ costs_X_tensor = torch.tensor(
     distance.cdist(X_source, X_target, metric='minkowski', p=hyper_parameter_p) ** hyper_parameter_p)
 
 
+prediction_change_happens_in_n_epochs = 0
 pred_y_target = None
 pred_y_target_tensor = None
 binary_pred_y_target = None
+# Tip: pred_y_target = [0.6, 0.3, 0.8] ---> binary_pred_y_target = [1,0,1]
 causal_distance = None
 transport_plan = None
 transport_plan_tensor = None
 approx_costs_Y_tensor = None
 approx_costs_tensor = None
 approx_causal_distance = None
+# Tip: We calculate causal_distance first with true costs and then calculate approx_causal_distance with approximate costs
+# causal_distance is our real target to minimize while approximate_causal_distance can give us gradient
+# the difference of causal_distance and approximate causal_distance lies only in costs. Their plans are the same, which is based on true costs
 
 for epoch in range(num_epochs):
-    print(f"Epoch {epoch}")
     model.train()
     new_pred_y_target_tensor = model(X_target_tensor)
     new_pred_y_target = new_pred_y_target_tensor.detach().numpy().reshape(-1)
     new_binary_pred_y_target = (new_pred_y_target>=0.5).astype(np.int64)
     if binary_pred_y_target is None or not np.array_equal(binary_pred_y_target, new_binary_pred_y_target):
-        print("One Calc of Causal")
+        prediction_change_happens_in_n_epochs += 1
         pred_y_target = new_pred_y_target
         pred_y_target_tensor = new_pred_y_target_tensor
         binary_pred_y_target = new_binary_pred_y_target
@@ -65,7 +73,11 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         approx_causal_distance.backward()
     optimizer.step()
-
-
-
+    if epoch % num_epochs_per_print == 0:
+        print(
+            f'Epoch [{epoch}/{num_epochs}]({prediction_change_happens_in_n_epochs}), '
+            f'True Loss: {causal_distance:.4f}, '
+            f'Approximate Loss: {approx_causal_distance.item():.4f}, '
+            f'Ideal Loss: {ideal_causal_distance:.4f}'
+        )
 
