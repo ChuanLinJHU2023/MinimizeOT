32c1c0a34b0938d82d1191b0dcf214a6d5a1bfac
Chuan Lin 2025 <1304404717@qq.com>
2025-07-02 14:58:43 -0400

    new

--- simple_examples/SimpleCalculate.py
+++ simple_examples/SimpleCalculate.py
@@ -5,11 +5,11 @@ from utils import *
 scaling_parameter_c = 128
 options = [('MIPGap', 0.2), ('TimeLimit', 30), ('MIPFocus', 1)]
 
-img = plt.imread('image_cats/A32.jpeg')
+img = plt.imread('../image_cats/A32.jpeg')
 img = downsample_image(img,2)
 img_A = np.array(img)
 
-img = plt.imread('image_cats/B32.jpeg')
+img = plt.imread('../image_cats/B32.jpeg')
 img = downsample_image(img,2)
 img_B = np.array(img)
 
